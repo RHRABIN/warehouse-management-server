c3bb93f19dbfe5e17f5d7fda65e45e12e7195acf
unknown <rhrabin934602@gmail.com>
2022-05-07 13:20:58 +0600

    single api created for inventory

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // furniture
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.1bkvw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -26,6 +26,16 @@ async function run() {
             const items = await cursor.toArray();
 
             res.send(items)
+        })
+        // load single item
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) }
+            console.log(query)
+            const item = await furnitureItem.findOne(query);
+            console.log(item)
+            res.send(item);
         })
 
     }
